05cbd86c6d6dbdaea8513fcc74d9f6a48f46d4c7
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-17 10:12:57 +0700

    Updated main.js

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
 const authRouter = require("./auth");
-// const usersRouter = require("./users");
+const usersRouter = require("./users");
 const tasksRouter = require("./tasks");
 const subtasksRouter = require("./subtasks");
 
 const mainRouter = express.Router();
 mainRouter.use("/auth", authRouter);
-// mainRouter.use("/users", usersRouter);
+mainRouter.use("/users", usersRouter);
 mainRouter.use("/tasks", tasksRouter);
 mainRouter.use("/subtasks", subtasksRouter);
 
