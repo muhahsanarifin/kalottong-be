1d54873a0ca07b10b1864e547fe5c70c750b55d7
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-18 13:01:06 +0700

    Add memoryUpload.js

---
+++ src/middlewares/memoryUpload.js
@@ -0,0 +1,60 @@
+const multer = require("multer");
+const path = require("path");
+
+let limits = {
+  fileSize: 204800,
+};
+
+const fileFilter = (req, file, cb) => {
+  const extName = path.extname(file.originalname);
+  const allowedExt = /jpeg|jpg|png|webp/;
+  if (!allowedExt.test(extName)) {
+    return cb({
+      msg: "Only use allowed extension (JPEG, JPG, PNG WEB",
+    });
+  }
+  cb(null, true);
+};
+
+const memoryStorage = multer.memoryStorage();
+
+const memoryUpload = multer({
+  storage: memoryStorage,
+  fileFilter,
+  limits,
+});
+
+const errorHandler = (err, res, next) => {
+  // console.log("Error: ", err);
+  // if (err instanceof multer.MulterError) {
+  //   return res.status(500).json({
+  //     msg: "Upload error",
+  //   });
+  // }
+  if (err) {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      return res.status(400).json({
+        data: null,
+        msg: "File size it too large. Allowed file size less than equel to 2mb",
+      });
+    }
+    if (err.code === "LIMIT_FILE_COUNT") {
+      return res.status(400).json({
+        data: null,
+        msg: "Too many files",
+      });
+    }
+    if(err.code === "LIMIT_UNEXPECTED_FILE") {
+      return res.status(400).json({
+        data: null,
+        msg: "Unexpected field",
+      });
+    }
+  }
+  next();
+};
+
+module.exports = {
+  singleMemoryUpload: (fieldName) => memoryUpload.single(fieldName),
+  errorHandler,
+};
