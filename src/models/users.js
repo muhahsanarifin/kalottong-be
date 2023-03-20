e8f29ac7e0f6304d13e887363f2b6b8fdce86889
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-20 08:11:24 +0700

    Update users.js

--- src/models/users.js
+++ src/models/users.js
@@ -2,14 +2,14 @@ const db = require("../configs/postgre");
 
 const updateProfile = (payload, body) => {
   const { user_id } = payload;
-  const { firstname, lastname } = body;
+  const { firstname, lastname, gender_id } = body;
 
   return new Promise((resolve, reject) => {
     const query =
-      "update users set firstname = $2, lastname = $3, updated_at = $4 where id = $1 returning *";
+      "update users set firstname = $2, lastname = $3, gender_id = $4, updated_at = $5 where id = $1 returning *";
     db.query(
       query,
-      [user_id, firstname, lastname, new Date()],
+      [user_id, firstname, lastname, gender_id, new Date()],
       (error, result) => {
         if (error) {
           return reject(error);
@@ -44,7 +44,8 @@ const uploadImageProfile = (payload, file, body) => {
 const getProfile = (payload) => {
   const { user_id } = payload;
   return new Promise((resolve, reject) => {
-    const query = "select * from users where id = $1";
+    const query =
+      "select u.email, u.firstname, u.lastname, g.name as gender, u.image, r.name as role, u.created_at, u.updated_at, u.last_login, u.key_change_password from users u join roles r on u.role_id =r.id join genders g on u.gender_id = g.id where u.id = $1";
     db.query(query, [user_id], (error, result) => {
       if (error) {
         return reject(error);
