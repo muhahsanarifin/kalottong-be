d976eac7469caa455adc1342cea762b4f4bd447c
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-13 19:15:02 +0700

    add response.js

---
+++ src/helpers/response.js
@@ -0,0 +1,15 @@
+module.exports = {
+  ErrorResponse: class ErrorResponse {
+    constructor(msg) {
+      this.msg = msg;
+    }
+  },
+};
+
+// class ErrorResponse {
+//   constructor(msg) {
+//     this.msg = msg;
+//   }
+// }
+
+// module.exports = { ErrorResponse };
